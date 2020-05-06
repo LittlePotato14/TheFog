984bb23478b48605967a8797003f0ce66b04425f
Polina Tochilna <narkada@mail.ru>
2020-05-06 23:48:25 +0300

    danger zone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,6 +39,21 @@ public class PlayerController : MonoBehaviour
             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             dialogCanvas.GetComponent<MessageBoxScript>().Show("Уровень пройден.", true);
         }
+
+        // Коллизия с опасной точкой.
+        else if(other.gameObject.tag == "danger" && globals.Playing)
+        {
+            // Устанаваливаем начальные настройки.
+            var hero = GameObject.FindGameObjectsWithTag("hero")[0];
+            hero.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            hero.transform.rotation = Quaternion.identity;
+            hero.transform.Rotate(0f, 0f, globals.GetDegreeAngle(globals.startDirection));
+            hero.transform.position = globals.startPosition;
+            angleBar.SetAngle(0);
+
+            // Показываем уведомление.
+            dialogCanvas.GetComponent<MessageBoxScript>().Show("Вы попали в опасную зону.");
+        }
     }
 
     /// <summary>
